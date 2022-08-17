df518236a55b6cc57be0a43091eb697ac12ea438
Yura Yura <yuriydmytrukr@gmail.com>
2022-08-17 11:27:29 +0300

    added output of sum

--- Lecture _1/Scripts/script.js
+++ Lecture _1/Scripts/script.js
@@ -3,5 +3,7 @@ let diaryPrice = 90.2345;
 let bookPrice = 123.965;
 
 console.log(Math.max(penPrice, diaryPrice, bookPrice))
-
 console.log(Math.min(penPrice, diaryPrice, bookPrice))
+
+let sumPrice = penPrice + diaryPrice + bookPrice;
+console.log(sumPrice);
