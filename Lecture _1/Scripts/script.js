9c790c990661b79eacb56c2cea0fb547a37a8088
Yura Yura <yuriydmytrukr@gmail.com>
2022-08-17 11:23:34 +0300

    added output of maximum and minimum number from variables

--- Lecture _1/Scripts/script.js
+++ Lecture _1/Scripts/script.js
@@ -1,3 +1,7 @@
 let penPrice = 15.678;
 let diaryPrice = 90.2345;
 let bookPrice = 123.965;
+
+console.log(Math.max(penPrice, diaryPrice, bookPrice))
+
+console.log(Math.min(penPrice, diaryPrice, bookPrice))
