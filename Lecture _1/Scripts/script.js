c7e8cae5b0be1fb3c039f0d77950fa518d334b85
Yura Yura <yuriydmytrukr@gmail.com>
2022-08-17 11:32:01 +0300

    added output of floor sum

--- Lecture _1/Scripts/script.js
+++ Lecture _1/Scripts/script.js
@@ -5,5 +5,8 @@ let bookPrice = 123.965;
 console.log(Math.max(penPrice, diaryPrice, bookPrice))
 console.log(Math.min(penPrice, diaryPrice, bookPrice))
 
-let sumPrice = penPrice + diaryPrice + bookPrice;
-console.log(sumPrice);
+let sumOfPrice = penPrice + diaryPrice + bookPrice;
+console.log(sumOfPrice);
+
+let sumOfFloorPrices = Math.floor(penPrice) + Math.floor(diaryPrice) + Math.floor(bookPrice);
+console.log(sumOfFloorPrices);
