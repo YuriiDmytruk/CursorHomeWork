abfe2ae07643070f832e5fd2c3d4523640e1a5db
Yura Yura <yuriydmytrukr@gmail.com>
2022-08-24 13:34:04 +0300

    create function formateName

--- Lecture3/Scripts/script.js
+++ Lecture3/Scripts/script.js
@@ -14,3 +14,27 @@ function getMaxDigitNumber(inData){
   return maxValue;
 }
 
+// ----- Under construction
+function getPowOfNumber(number){
+  number = +number
+  if (!inData){
+    return 0;
+  }
+}
+
+function formateName(name){
+  name = String(name);
+  if (!name){
+    return "";
+  }
+
+  let letters = name.split('');
+
+  letters[0] = letters[0].toUpperCase();
+  for(let i = 1; i < letters.length; i++){
+    letters[i] = letters[i].toLowerCase();
+  }
+  return letters.join('');
+}
+
+console.log(formateName("1DAAsdVV"))
