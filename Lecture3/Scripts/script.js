7b3afd5c79bc4f7bf831a8c88df0071c070d2f1c
Yura Yura <yuriydmytrukr@gmail.com>
2022-08-24 13:43:21 +0300

    create function getPowOfNUmber

--- Lecture3/Scripts/script.js
+++ Lecture3/Scripts/script.js
@@ -1,12 +1,12 @@
-function getMaxDigitNumber(inData){
+function getMaxDigitNumber(inData) {
   inData = String(inData);
-  if (!+inData){
+  if (!+inData) {
     return 0;
   }
-  let numbers = inData.split('');
+  let numbers = inData.split("");
   let maxValue = +numbers[0];
-  for (let i = 1; i < numbers.length; i++){
-    if(maxValue < +numbers[i]){
+  for (let i = 1; i < numbers.length; i++) {
+    if (maxValue < +numbers[i]) {
       maxValue = +numbers[i];
     }
   }
@@ -14,27 +14,34 @@ function getMaxDigitNumber(inData){
   return maxValue;
 }
 
-// ----- Under construction
-function getPowOfNumber(number){
-  number = +number
-  if (!inData){
+function getPowOfNumber(number, pow) {
+  if (!+number || !+pow) {
     return 0;
   }
+  if (pow == 0){
+    return 1;
+  }
+  let result = number;
+  for (let i = 1; i < pow; i++){
+    result *= number;
+  }
+
+  
+  return result;
 }
 
-function formateName(name){
+function formateName(name) {
   name = String(name);
-  if (!name){
+  if (!name) {
     return "";
   }
 
-  let letters = name.split('');
+  let letters = name.split("");
 
   letters[0] = letters[0].toUpperCase();
-  for(let i = 1; i < letters.length; i++){
+  for (let i = 1; i < letters.length; i++) {
     letters[i] = letters[i].toLowerCase();
   }
-  return letters.join('');
+  return letters.join("");
 }
 
-console.log(formateName("1DAAsdVV"))
