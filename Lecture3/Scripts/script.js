eb3ea64d9125804e32ed3849894f3331a4322a55
Yura Yura <yuriydmytrukr@gmail.com>
2022-08-24 14:28:31 +0300

    create function deleteLetterInWord

--- Lecture3/Scripts/script.js
+++ Lecture3/Scripts/script.js
@@ -18,15 +18,13 @@ function getPowOfNumber(number, pow) {
   if (!+number || !+pow) {
     return 0;
   }
-  if (pow == 0){
+  if (pow == 0) {
     return 1;
   }
   let result = number;
-  for (let i = 1; i < pow; i++){
+  for (let i = 1; i < pow; i++) {
     result *= number;
   }
-
-  
   return result;
 }
 
@@ -45,62 +43,73 @@ function formateName(name) {
   return letters.join("");
 }
 
-function calculateSalaryWithoutTax(salary, tax){
+function calculateSalaryWithoutTax(salary, tax) {
   if (!+salary || !+tax) {
     return 0;
   }
-  let taxSum = (salary / 100) * tax
+  let taxSum = (salary / 100) * tax;
   return salary - taxSum;
 }
 
-function getRandomNumberFromNtoM(n, m){
+function getRandomNumberFromNtoM(n, m) {
   if (!+n || !+m) {
     return 0;
   }
-  return Math.floor(Math.random() * (m - n + 1) + n)
+  return Math.floor(Math.random() * (m - n + 1) + n);
 }
 
-function countRepeatInWord(letter, word){
+function countRepeatInWord(letter, word) {
   word = String(word);
   if (!letter || !word) {
     return 0;
   }
-  let letters = word.split('');
+  let letters = word.split("");
   let count = 0;
-  for (let i = 0; i < letters.length; i++){
-    if (letters[i].toUpperCase() == letter.toUpperCase()){
+  for (let i = 0; i < letters.length; i++) {
+    if (letters[i].toUpperCase() == letter.toUpperCase()) {
       count++;
     }
   }
   return count;
 }
 
-function convertMoney(inData){
+function convertMoney(inData) {
   inData = String(inData).toUpperCase();
   if (!inData) {
     return 0;
   }
-  if (inData.includes('$')){
-    number = +inData.replace('$', '');
+  if (inData.includes("$")) {
+    number = +inData.replace("$", "");
     return number * 25;
-  }
-  else if (inData.includes('UAH')){
-    number = +inData.replace('UAH', '');
+  } else if (inData.includes("UAH")) {
+    number = +inData.replace("UAH", "");
     return number / 25;
+  } else {
+    return 0;
   }
-  else {return 0;}
 }
 
-function createPassword(charNumber = 8){
+function createPassword(charNumber = 8) {
   if (!+charNumber) {
     return 0;
   }
   let password = "";
-  for (let i = 0; i < charNumber; i++){
+  for (let i = 0; i < charNumber; i++) {
     password += Math.floor(Math.random() * 10).toString();
   }
   return password;
 }
 
-console.log(createPassword())
+function deleteLetterInWord(letter, word) {
+  word = String(word);
+  letter = String(letter);
+  if (!letter || !word) {
+    return 0;
+  }
+  for (let i = 0; i < word.length; i++) {
+    word = word.replace(letter, "");
+  }
+  return word;
+}
 
+console.log(deleteLetterInWord("a", "blablabla"));
