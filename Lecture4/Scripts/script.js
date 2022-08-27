0091caea2bc11fb90428a3479f6c4f3f580a6158
Yura Yura <yuriydmytrukr@gmail.com>
2022-08-27 11:19:12 +0300

    create getThemes getMarks and getRadonMark fuctions

--- Lecture4/Scripts/script.js
+++ Lecture4/Scripts/script.js
@@ -8,16 +8,25 @@ const marks = [4, 5, 5, 3, 4, 5];
 
 const pairs = getPairs(students);
 const themesPairs = getThemes(pairs, themes)
+const studentsMarks = getMarks(students, marks);
+const randomMark = getRanndomMark(themesPairs, 1, 5);
 
-console.log(pairs);
+console.log(randomMark);
 
 function getPairs(students) {
   const girls = students.filter((e) => e.charAt(e.length - 1) === "a");
   const boys = students.filter((e) => e.charAt(e.length - 1) != "a");
-  const pairs = girls.map(e => [e, boys[girls.indexOf(e)]]);
-  return pairs;
+  return girls.map(e => [e, boys[girls.indexOf(e)]]);
 }
 
 function getThemes(pairs, themes){
-  
+  return pairs.map(e => [e.join(" i "), themes[pairs.indexOf(e)]]);
+}
+
+function getMarks(students, marks){
+  return students.map(e => [e, marks[students.indexOf(e)]])
+}
+
+function getRanndomMark(themesPairs, min, max){
+return themesPairs.map(e => e.concat(Math.floor(Math.random() * (max - min + 1) + min)));
 }
