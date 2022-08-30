43c0f33c30db446dc98f0bd38a2fe7dd3114dae3
Yura Yura <yuriydmytrukr@gmail.com>
2022-08-30 19:00:27 +0300

    add function getModa

--- Lecture5/Scripts/script.js
+++ Lecture5/Scripts/script.js
@@ -1,4 +1,24 @@
-function getRandomArray (lenght, min, max){
-  return new Array(lenght).fill(0).map(e => Math.floor(Math.random() * (max - min + 1) + min));
+function getRandomArray(lenght, min, max) {
+  return new Array(lenght)
+    .fill(0)
+    .map((e) => Math.floor(Math.random() * (max - min + 1) + min));
 }
-console.log(getRandomArray(10, 1, 10));
+
+function getModa(...arr) {
+  arr = arr.filter((e) => Number.isInteger(e));
+  let moda = arr[0],
+    maxCount = 0,
+    count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    count = arr.filter(e => e === arr[i]).length;
+    if (maxCount < count) {
+      moda = arr[i];
+      maxCount = count;
+    }
+  }
+  return moda;
+}
+
+console.log(
+  getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)
+);
