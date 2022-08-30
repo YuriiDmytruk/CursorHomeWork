275d3736358e8bca8320d46592a862a99dfdb701
Yura Yura <yuriydmytrukr@gmail.com>
2022-08-30 19:16:33 +0300

    add getMedian and filterEvenNumbers functions

--- Lecture5/Scripts/script.js
+++ Lecture5/Scripts/script.js
@@ -10,7 +10,7 @@ function getModa(...arr) {
     maxCount = 0,
     count = 0;
   for (let i = 0; i < arr.length; i++) {
-    count = arr.filter(e => e === arr[i]).length;
+    count = arr.filter((e) => e === arr[i]).length;
     if (maxCount < count) {
       moda = arr[i];
       maxCount = count;
@@ -19,6 +19,25 @@ function getModa(...arr) {
   return moda;
 }
 
-console.log(
-  getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)
-);
+function getAverage(...arr) {
+  return (
+    arr
+      .filter((e) => Number.isInteger(e))
+      .reduce((prev, cur) => prev + cur, 0) / arr.length
+  );
+}
+
+function getMedian(...arr) {
+  arr = arr.filter((e) => Number.isInteger(e)).sort((a, b) => a - b);
+  const mid = Math.floor(arr.length / 2);
+  return arr.length % 2 === 0 ? (arr[mid] + arr[mid - 1]) / 2 : arr[mid];
+}
+
+function filterEvenNumbers(...arr){
+  return arr.filter(e => e % 2 != 0);
+}
+
+
+
+
+console.log(filterEvenNumbers(1, 2, 3, 4, 5, 6));
