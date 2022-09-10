dccd27241a1a072a363b7cbeb31e0796b965b127
Yura Yura <yuriydmytrukr@gmail.com>
2022-09-10 11:11:49 +0300

    create Recover and Dismiss function

--- Lecture8/Scripts/script.js
+++ Lecture8/Scripts/script.js
@@ -1,27 +1,59 @@
 class Student {
-  constructor(university, course, fullName, marks){
+  constructor(university, course, fullName, marks) {
     this.university = university;
     this.course = course;
     this.fullName = fullName;
     this.marks = marks;
+    this.excluded = false;
   }
 
-  getInfo(){
+  getInfo() {
     return `Студент ${this.course}го курсу ${this.university}, ${this.fullName}`;
   }
 
-  get marks(){
-    return this._marks;
+  get marks() {
+    if (!this.excluded) {
+      return this._marks;
+    }
+    return null;
   }
 
-  set marks(value){
-    this._marks = Array.isArray(this.marks) ? this.marks : [];
-    this._marks.push(value);
-    this._marks = this._marks.flat(Infinity);
-    return this._marks;
+  set marks(value) {
+    if (!this.excluded) {
+      this._marks = Array.isArray(this.marks) ? this.marks : [];
+      this._marks.push(value);
+      this._marks = this._marks.flat(Infinity);
+      return this._marks;
+    }
+    return null;
+  }
+
+  getAverageMark() {
+    return (
+      this.marks.reduce((partialSum, a) => partialSum + a, 0) /
+      this.marks.length
+    );
+  }
+
+  dismiss() {
+    this.excluded = true;
+  }
+
+  recover() {
+    this.excluded = false;
   }
 }
 
-const student = new Student("Вищої Школи Психотерапії м.Одеса", 1, "Остап Родоманський Бендер", [1,2,3,4,5]);
+class BudgetStudent extends Student {
+  constructor(university, course, fullName, marks, scholarShip) {
+    super(university, course, fullName, marks);
+    this.scholarShip = scholarShip;
+  }
+}
 
-student.marks = 5;
+const student = new Student(
+  "Вищої Школи Психотерапії м.Одеса",
+  1,
+  "Остап Родоманський Бендер",
+  [5, 4, 4, 5, 5]
+);
