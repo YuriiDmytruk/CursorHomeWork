fc594084b17c1b25520c815ba7422924e10933d7
Yura Yura <yuriydmytrukr@gmail.com>
2022-09-10 10:05:58 +0300

    Write get and set for marks field

--- Lecture8/Scripts/script.js
+++ Lecture8/Scripts/script.js
@@ -1,15 +1,23 @@
 class Student {
-  constructor(university, course, fullName){
+  constructor(university, course, fullName, marks){
     this.university = university;
     this.course = course;
     this.fullName = fullName;
+    this.marks = marks;
   }
 
   getInfo(){
     return `Студент ${this.course}го курсу ${this.university}, ${this.fullName}`;
   }
+
+  get getMarks(){
+    return this.marks;
+  }
+
+  set setMarks(value){
+    this.marks = value;
+  }
 }
 
-const student = new Student("Вищої Школи Психотерапії м.Одеса", 1, "Остап Родоманський Бендер")
-
-console.log(student.getInfo());
+const student = new Student("Вищої Школи Психотерапії м.Одеса", 1, "Остап Родоманський Бендер", [1,2,3,4,5]);
+console.log(student.marks);
