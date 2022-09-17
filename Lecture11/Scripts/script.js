20822c6785ea1d5e3343eadc062490150fc6ad86
DESKTOP-5015UJ0\Roman <rdmytruk78@gmail.com>
2022-09-17 16:48:46 +0300

    Finish Lab 11

--- Lecture11/Scripts/script.js
+++ Lecture11/Scripts/script.js
@@ -1,5 +1,12 @@
-addEventListener("keypress", onKeyPressed);
 let createdKeys = [];
+let audioBindList = [];
+let audioListIndex = -1;
+
+const musicList = ["1", "2", "3"];
+const audioList = addAudio(musicList);
+
+addEventListener("keypress", onKeyPressed);
+addEventListener("click", onClickSetActive);
 
 function onKeyPressed(e) {
   if (checkIfUnique(e, createdKeys)) {
@@ -7,16 +14,31 @@ function onKeyPressed(e) {
     createKey(e);
     createdKeys.push(e);
   } else {
+    disableKeys(createdKeys);
     setActive(e, createdKeys);
   }
 }
 
-function setActive(e, createdKeys) {
-  for (let i = 0; i < createdKeys.length; i++) {
-    if (e.key.toUpperCase() == createdKeys[i].key.toUpperCase()) {
-      console.log(createdKeys[i]);
-    }
+function onClickSetActive(e) {
+  if (e.target.className === "passive") {
+    disableKeys(createdKeys);
+    e.target.className = "active";
+    audioList[
+      audioBindList.find((el) => el.elementKey.toUpperCase() == e.target.id)
+        .audioListIndex
+    ].play();
   }
+}
+
+function setActive(e) {
+  document.getElementById(e.key.toUpperCase()).className = "active";
+  console.log(e);
+  console.log(e.key);
+  audioList[
+    audioBindList.find(
+      (el) => el.elementKey.toUpperCase() == e.key.toUpperCase()
+    ).audioListIndex
+  ].play();
 }
 
 function checkIfUnique(e, createdKeys) {
@@ -35,6 +57,9 @@ function createKey(e) {
   key.id = e.key.toUpperCase();
   key.textContent = e.key.toUpperCase();
   wrapper.appendChild(key);
+  const index = getAudioListndex();
+  audioBindList.push(new AudioBind(e.key.toUpperCase(), index));
+  audioList[index].play();
 }
 
 function disableKeys() {
@@ -44,5 +69,28 @@ function disableKeys() {
 
   for (let i = 0; i < elementsWithActiveClass.length; i++) {
     elementsWithActiveClass[i].className = "passive";
+  }
+}
+
+function addAudio(musicList) {
+  let result = [];
+  musicList.forEach((element) => {
+    result.push(new Audio(`./Music/${element}.mp3`));
+  });
+  return result;
+}
+
+function getAudioListndex() {
+  audioListIndex += 1;
+  if (audioListIndex === audioList.length) {
+    audioListIndex = 0;
+  }
+  return audioListIndex;
+}
+
+class AudioBind {
+  constructor(elementKey, audioListIndex) {
+    this.elementKey = elementKey;
+    this.audioListIndex = audioListIndex;
   }
 }
