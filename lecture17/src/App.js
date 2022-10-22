a73d31fb2806496bd63250c489d0a5bde5eb9905
Yura Yura <yuriydmytrukr@gmail.com>
2022-10-22 16:00:14 +0300

    Lab 17 done

--- lecture17/src/App.js
+++ lecture17/src/App.js
@@ -2,9 +2,19 @@
 import Post from './Scripts/Post.js'
 
 function App() {
+  const ANAKIN_IMAGE = "https://imagez.tmz.com/image/b8/1by1/2021/10/25/b823cb5212504585a16a46faa55537db_xl.jpg";
+  const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
   return (
     <div>
-      <Post name="Yura"/>
+      <Post author={{
+            name: "Anakin skywalker",
+            photo: ANAKIN_IMAGE,
+            nickname: "@dart_vader"
+         }}
+         content="WTF? Who is Ray? Why she is Skywalker? Luke...?"
+         image={RAY_IMAGE}
+         date={"26 feb."}
+/>
     </div>
   );
 }
--- lecture17/src/Scripts/Post.js
+++ lecture17/src/Scripts/Post.js
@@ -1,7 +1,14 @@
 import '../Styles/Post.css';
 
 function Post(prop){
-  return <div className='Post'>Post, {prop.name}</div>
+  return <div className='Post'>
+    <p>{prop.author.name}</p>
+    <p>{prop.author.nickname}</p>
+    <img src={prop.author.photo} alt="Author"></img>
+    <p>{prop.content}</p>
+    <img src={prop.image} alt="Post"></img>
+    <p>{prop.date}</p>
+  </div>
 }
 
 export default Post;
