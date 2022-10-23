8269f64046b29009fe4a025f072139cfe8ccb360
Yura Yura <yuriydmytrukr@gmail.com>
2022-10-23 10:18:17 +0300

    Lab17 done

--- lecture17/src/Scripts/Post.js
+++ lecture17/src/Scripts/Post.js
@@ -2,9 +2,9 @@ import '../Styles/Post.css';
 
 function Post(prop){
   return <div className='Post'>
-    <p>{prop.author.name}</p>
-    <p>{prop.author.nickname}</p>
     <img src={prop.author.photo} alt="Author"></img>
+    <div>{prop.author.name}</div>
+    <div>{prop.author.nickname}</div>
     <p>{prop.content}</p>
     <img src={prop.image} alt="Post"></img>
     <p>{prop.date}</p>
